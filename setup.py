4848fa8a2b089b8e3aaf63d1590ad1af33b09112
Ian Horsman <ianphorsman@gmail.com>
2018-01-12 01:51:52 -0500

    Added import to init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='another_neural_net',
-    description='Packaged fully connected, multilayer neural network written in numpy with data_recipes for testing with sklearn datasets.',
+    description='Packaged fully connected, multilayer neural network written in numpy with data_recipes for testing with sklearn datasets available on github.',
     url='https://github.com/Ianphorsman/AnotherNeuralNetwork',
     author='Ian Horsman',
     author_email='ianphorsman@gmail.com',
