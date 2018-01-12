49b84f204d54e96907c4d5f9827f98e48c8249e5
Ian Horsman <ianphorsman@gmail.com>
2018-01-12 01:39:16 -0500

    Removed unused imports

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # AnotherNeuralNetwork
 
 data recipes contain jupyter notebooks
+
+X, y = make_moons(1000, noise=0.4, random_state=333)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+mlp = MLP(X_train, y_train, iterations=1500, learning_rate=0.003)
+
+mlp.train(mlp.X, mlp.y)
+
+print(mlp.test_accuracy(X_test, np.atleast_2d(y_test)))
+
+mlp.plot_decision_boundary(X_test[:, 0], X_test[:, 1], y_test)
+mlp.plot_performance()
--- numpy_neural_nets/basic_mlp.py
+++ numpy_neural_nets/basic_mlp.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import time
 import _pickle as picklerick
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons, make_circles
-import pdb
 
 
 class MLP(object):
@@ -161,17 +159,4 @@ class MLP(object):
 
     def inspect_performance(self,iteration, cost, accuracy):
         return "Iteration: {} , Cost: {} , Accuracy: {}".format(iteration, cost, accuracy)
-
-
-X, y = make_moons(1000, noise=0.4, random_state=333)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-mlp = MLP(X_train, y_train, iterations=1500, learning_rate=0.003)
-
-mlp.train(mlp.X, mlp.y)
-
-print(mlp.test_accuracy(X_test, np.atleast_2d(y_test)))
-
-mlp.plot_decision_boundary(X_test[:, 0], X_test[:, 1], y_test)
-mlp.plot_performance()
 
